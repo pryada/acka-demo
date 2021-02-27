5cc88a728f9b61690bf7939764edecf0b3b9345a
Sergey Pryada <pryada@gmail.com>
2021-02-27 11:48:09 +0600

    rewrite group-by to arrow fn style

--- src/helpers/group-by.js
+++ src/helpers/group-by.js
@@ -1,10 +1,10 @@
-export default function GroupBy(key) {
-    return function group(array) {
-        return array.reduce((acc, obj) => {
-            const property = obj[key];
-            acc[property] = acc[property] || [];
-            acc[property].push(obj);
-            return acc;
-        }, {});
-    };
-}
+const GroupBy = (key) => (array) => {
+    return array.reduce((acc, obj) => {
+        const property = obj[key];
+        acc[property] = acc[property] || [];
+        acc[property].push(obj);
+        return acc;
+    }, {});
+};
+
+export default GroupBy;
